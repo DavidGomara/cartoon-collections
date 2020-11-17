86dc7980b5605018498e115924c07789956ca874
David Gomara <davidgomara7@gmail.com>
2020-11-17 03:24:15 +0000

    commit tree aef21638984526ab794841446d17138118673551

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  x = 1
+  array.each do |name|
+    puts "#{x}. #{name}"
+    x += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.each do |word|
+    new_array << word.capitalize + "!"
+  end 
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |words|
+    words.length > 4
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  new_array = []
+  array.collect do |food|
+    if cheese_types.include?(food)
+      new_array.push(food)
+    end
+  end
+  if new_array.length > 0
+    new_array.first
+  else 
+    puts "no cheese"
+  end 
 end
+
